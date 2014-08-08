2e85d656c3959753a2107d8884cf896815ae807d
Igor Mironov <mcs6502-sek@yahoo.com.au>
2014-08-09 09:08:50 +1000

    fix the non_empty? method to use encoding-specific regexps  (note that the original test cannot use UTF-7 on my ruby)

--- lib/regexp_m17n.rb
+++ lib/regexp_m17n.rb
@@ -1,5 +1,19 @@
 module RegexpM17N
   def self.non_empty?(str)
-    str =~ /^.+$/
+    enc = str.encoding
+    if enc.dummy? # cannot create regexps for dummy encodings, so encode the string instead
+      static_regexp = /^.+$/
+      static_regexp =~ str.encode(static_regexp.encoding)
+    else # otherwise create use an encoding-specific method that will use its own regexp
+      unless enc.respond_to?(:non_empty_str?)
+        regexp = Regexp.new('^.+$'.encode(enc))
+        enc.instance_eval do
+          define_singleton_method(:non_empty_str?) do |str|
+            regexp =~ str
+          end
+        end
+      end
+      enc.non_empty_str?(str)
+    end
   end
 end
--- test/regexp_m17n_test.rb
+++ test/regexp_m17n_test.rb
@@ -8,4 +8,32 @@ class RegexpTest < MiniTest::Unit::TestCase
       assert(RegexpM17N.non_empty?('.'.encode(enc)))
     end
   end
+
+  def test_non_empty_string_using_all_code_converters_we_could_find
+    Encoding.list.each do |enc|
+      str = encode('.', enc)
+      if str
+        assert(RegexpM17N.non_empty?(str))
+      else
+        puts "No code converter for #{enc}"
+      end
+    end
+
+    # Run through all encodings once again to verify that the non_empty? method works for multiple invocations.
+    Encoding.list.each do |enc|
+      str = encode('.', enc)
+      if str
+        assert(RegexpM17N.non_empty?(str))
+        assert(RegexpM17N.non_empty?(str))
+      end
+    end
+  end
+
+  def encode(str, enc)
+    begin
+      str.encode(enc)
+    rescue Encoding::ConverterNotFoundError => e
+      nil
+    end
+  end
 end
